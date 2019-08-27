21ecd8332bc17b41d00817302648768d6d5b572f
TBrockster <tobybrock93@gmail.com>
2019-08-27 15:44:52 +0100

    restructured withdraw method, tested and passed

--- lib/bank_interface.rb
+++ lib/bank_interface.rb
@@ -4,7 +4,7 @@
 class BankInterface
   def initialize
     @balance = 0
-    @statement_history = [["date || credit || debit || balance"]]
+    @statement_history = [['date || credit || debit || balance']]
   end
 
   def print_statement
@@ -13,11 +13,11 @@ class BankInterface
 
   def deposit(amount_to_deposit, date_deposited)
     @balance += amount_to_deposit
-    @statement_history << ["#{date_deposited} || #{'%.2f' % amount_to_deposit} || || #{'%.2f' % @balance}"]
+    @statement_history << ["#{date_deposited} || #{format('%.2f', amount_to_deposit)} || || #{format('%.2f', @balance)}"]
   end
 
   def withdraw(amount_to_withdraw, date_deposited)
-    @balance -= amount_to_deposit
-    @statement_history << [date_deposited, ' ', amount_to_withdraw, @balance]
+    @balance -= amount_to_withdraw
+    @statement_history << ["#{date_deposited} || || #{format('%.2f', amount_to_withdraw)} || #{format('%.2f', @balance)}"]
   end
 end
--- spec/units/bank_interface_spec.rb
+++ spec/units/bank_interface_spec.rb
@@ -5,10 +5,14 @@ require 'bank_interface'
 describe BankInterface do
   it { is_expected.to respond_to(:deposit).with(2).arguments }
   it { is_expected.to respond_to(:withdraw).with(2).arguments }
-  describe '#deposit' do
-    it 'adds deposit to balance and returns the correct format' do
-      subject.deposit(50, '27/08/2019')
-      expect(subject.print_statement). to eq("date || credit || debit || balance\n27/08/2019 || 50.00 || || 50.00")
-    end
+
+  it 'adds deposit to balance and returns the correct format' do
+    subject.deposit(50, '27/08/2019')
+    expect(subject.print_statement). to eq("date || credit || debit || balance\n27/08/2019 || 50.00 || || 50.00")
+  end
+
+  it 'withdaws amount from balance and returns the correct format' do
+    subject.withdraw(50, '31/10/2019')
+    expect(subject.print_statement). to eq("date || credit || debit || balance\n31/10/2019 || || 50.00 || -50.00")
   end
 end
